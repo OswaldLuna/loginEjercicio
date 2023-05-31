71562682d0ff21d0b0fff020caba930f64098da0
OswaldoL <oswaldo.luna.dev@gmail.com>
2023-05-31 14:37:31 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let nombre = 'olh';
+alert(nombre);
+
+const contenido = document.getElementById("contenido");
+contenido.innerHTML = " hola programdores";
+
+
+let edad = 29;
+
+const COLOR_ROJO = '#FF0000';
